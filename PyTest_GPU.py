60b88b5bb7e07f2a28a09c399f0475b7f9978811
wz <33210252+WangZhen1175701153@users.noreply.github.com>
2021-11-03 18:38:35 +0800

    Update PyTest_GPU.py

--- PyTest_GPU.py
+++ PyTest_GPU.py
@@ -6,9 +6,10 @@ try:
     import torch
 except:
     print("当前环境下没有正确安装Torch")
-
+import sys
 
 def mainTest():
+    print("当前python版本为:{}".format(sys.version))
     print("tensorflow版本为:{}".format(tf.__version__))
     print("torch版本为{}".format(torch.__version__))
 
